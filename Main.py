3ce641ad1438b606dab9f4b0528dce0fa3229036
EragonRD <56035685+EragonRD@users.noreply.github.com>
2024-10-09 15:14:54 +0200

    verif if  file aleady exist

--- Main.py
+++ Main.py
@@ -1,11 +1,11 @@
-import Combat 
+import Combat  as Combat
 from Pokemon import Pokemon
-import Get_ability
+from Get_ability import get_ability
 
-picka = Pokemon("Pickachu","Electrique",154,91,115,113,113,93,50)
-salam = Pokemon("Salamche","Feu",154,91,115,113,113,93,50)
+#picka = Pokemon("Pickachu","Electrique",154,91,115,113,113,93,50)
+#salam = Pokemon("Salamche","Feu",154,91,115,113,113,93,50)
 
 nom_pokemon = input("choose your pokemon")
 
-Combat.lanceAttack(picka,salam)
-Get_ability.get_Ability(nom_pokemon)
+#Combat.lanceAttack(picka,salam)
+get_ability(nom_pokemon)
