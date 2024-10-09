16515fe99852174a19dc484d734470888c1a8960
AlexGabrielC <chitu.a99@gmail.com>
2024-10-09 15:47:36 +0200

    new main

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 import Combat 
 from Pokemon import Pokemon
-import Get_ability
+from Get_ability import get_ability 
 import json
 
 picka = Pokemon("pickachu","Electrique",154,91,115,113,113,93,50)
@@ -8,13 +8,21 @@ salam = Pokemon("Salamche","Feu",154,91,115,113,113,93,50)
 
 #charge les données des pokemon nom et stats lvl 1
 with open('pokemon_stats_level_1.json', "r") as poke: 
-    print(json.load(poke))
+    pokemon_names = [pokemon["name"] for pokemon in json.load(poke).values()]
 
-print(poke)
+print(pokemon_names)
 
-nom_pokemon = input("choose your pokemon")
-
-
+while True:
+        nom_pokemon = input("Choose your Pokémon: ").strip().lower()  # On met en minuscule pour faciliter la comparaison
+        if nom_pokemon in pokemon_names :  # On compare avec les noms 
+            print(f"Vous avez choisi {nom_pokemon.capitalize()}!")
+            break
+        else:
+            print("Nom invalide, veuillez choisir un Pokémon valide.")
+print("downloading pokemon data")
+get_ability(nom_pokemon)
+print("done")
+"""
 Combat.lanceAttack(picka,salam)
-Get_ability.get_ability(nom_pokemon)
 picka.apprendreCapacite()
+"""
