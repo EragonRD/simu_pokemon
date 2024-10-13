bfb5ffc303957caa858af411e2442db6aaf11efd
EragonRD <debbah.rabah.amir@gmail.com>
2024-10-13 21:23:30 +0200

    tout propre le main et le combat

--- Combat.py
+++ Combat.py
@@ -6,9 +6,7 @@ class Combat:
     def __init__(self,poke_1: Pokemon, poke_2: Pokemon):
         self.poke_1 = poke_1
         self.poke_2 = poke_2
-        self.engagement()
-        self.info = ""
-
+        self.tour = 0
 
     def get_pok_1(self):
         return self.poke_1
@@ -16,17 +14,6 @@ class Combat:
     def get_pok_2(self):
         return self.poke_2
     
-    def get_capacites(self):
-        if self.tour:
-            return self.poke_2.get_capacites()
-        else :
-            return self.poke_1.get_capacites()
-    
-    def engagement(self):
-        if(self.poke_1.get_vit()<self.poke_2.get_vit()):
-            self.tour = 1
-        else :
-            self.tour = 0
     
     def lance_combat(self):
         """
@@ -34,52 +21,32 @@ class Combat:
         CM etant le bonus (element) soit type vs type 
         """
         #decide de l'attaquand en fonction de la vitesse
-        # print(self.poke_1.get_vit())
+        print(self.poke_1.get_vit())
+        if(self.poke_1.get_vit()<self.poke_2.get_vit()):
+            p1 = self.poke_1
+            p2 = self.poke_2
+        else :
+            p1 = self.poke_2
+            p2 = self.poke_1 
         
-        
+        cm = calcul_cm(p1.get_elem(),p2.get_elem())
         #chacun sont tour celon la boucle 1,0,1 
         if (self.tour):
-            cm = calcul_cm(self.poke_2.get_elem(),self.poke_1.get_elem())
-            self.info = self.poke_2.get_nom() + " se prepare pour lancer une attaque"
-            # print(f"Vous attaquez avec {attaque} !")
-            # lance_attack(p1,p2,attaque)
+            print(p1.get_nom()+" se prepare pour lancer une attaque")
+            print(repr(p1))
+            attaque = p1.get_capacites()[choix_de_la_capacite()]
+            print(f"Vous attaquez avec {attaque} !")
+            lance_attack(p1,p2,attaque)
         else :
-            cm = calcul_cm(self.poke_1.get_elem(),self.poke_2.get_elem())
-            self.info = self.poke_1.get_nom() + " se prepare pour lancer une attaque"
-            # print(f"Vous attaquez avec {attaque} !")
-            # lance_attack(p2,p1,attaque)
-
-    def lance_attaque(self, attaque):
-        if (self.tour):
-            self.info = self.poke_2.get_nom() + " attaque avec {attaque}"
-            lance_attack(attaque, self.poke_2, self.poke_1)
-        else :
-            self.info = self.poke_1.get_nom() + " attaque avec {attaque}"
-            lance_attack(attaque, self.poke_1, self.poke_2)
-        self.tour = 1 - self.tour
-
+            print(p2.get_nom()+" se prepare pour lancer une attaque")
+            print(repr(p2))
+            attaque = p2.get_capacites()[choix_de_la_capacite()]
+            print(f"Vous attaquez avec {attaque} !")
+            lance_attack(p2,p1,attaque)
     
-
-def lance_attack(attaque, p1, p2):
-    """
-    prend un pokemon attaquant p1 attaquant et p2 defenseur 
-    """
-    if (attaque == None):
-        print("ne pouvant pas lancez un sort notre pokemon s'elance a pleine vitesse tete la premiere")
-        degat = math.floor((math.floor(math.floor(((math.floor(int(p1.get_niveau()) * 0.4 + 2) * p1.get_atk_n() * 150) / p2.get_def_n()) / 50)) + 2) * 1)
-        p2.set_hp(p2.get_hp()- degat)
-        p1.set_hp(p1.get_hp()- int(degat/4))
-        print("une attaque de "+str(degat) +" degats")
-    # attaque physique
-    elif (attaque.get_categorie() == 0 and attaque.get_cible() == 1): #verifiez si les conditions sont correctent
-        #transforme les hp du defenseur 
-        p2.set_hp(p2.get_hp()-math.floor((math.floor(math.floor(((math.floor(p1.get_niveau() * 0.4 + 2) * p1.get_atk_n * attaque.get_puissance())/p2.get_def_n)/50)) +2)*cm))
-    # attaque special
-    elif (attaque.get_categorie() == 0 and attaque.get_cible() == 1): #verifiez si les conditions sont correctent
-        #transforme les hp du defenseur 
-        p2.set_hp(p2.get_hp()-math.floor((math.floor(math.floor(((math.floor(p1.get_niveau() * 0.4 + 2) * p1.get_atk_spe * attaque.get_puissance())/p2.get_def_spe)/50)) +2)*cm))
-    else :
-        print("attaque echoué, better luck next time")
+        self.tour = 1 - self.tour
+        print(str(self.poke_1))
+        print(str(self.poke_2))
 
 
 
@@ -151,35 +118,26 @@ def choix_de_la_capacite():
 
 
 
-
-    
-
-
-
-"""
-
-def lanceAttack(attaquant :type[Pokemon],defenseur: type[Pokemon]):
-        print(attaquant.capacites)
-        try :
-            print(str(attaquant))
-            print(repr(attaquant))
-            chose = int(input ("Chose a capacity : "))
-            print(str(i) + " :" + "\n".join(str(attaquant.capacites[i])) for i in range(4))
-            
-        except ValueError:
-            print(f"la velur entrée n'est pas valide")
-        cm = calcul_cm(attaquant.element,defenseur.element)
-        capacite = attaquant.capacites[chose]
-        if(capacite==None):
-            print("cant attack sorry next turn")
-            return "non non non"
-        # attaque physique
-        if capacite.categorie == 0 and capacite.cible == 1 :
-            defenseur.vie = defenseur.vie - math.floor((math.floor(math.floor(((math.floor(attaquant.lvl * 0.4 + 2) * attaquant.attack * capacite.get_puissance())/attaquant.defense)/50)) +2)*cm)
-        # attaque spéciale
-        if capacite.categorie == 1 and capacite.cible == 1 : 
-            defenseur.vie = defenseur.vie - math.floor((math.floor(math.floor(((math.floor(attaquant.lvl * 0.4 + 2) * attaquant.attSpecial * capacite.get_puissance())/attaquant.defSpecial)/50)) +2)*cm)
+#pas d'attaque
+def lance_attack(p1,p2,attaque):
+    """
+    prend un pokemon attaquant p1 attaquant et p2 defenseur 
+    """
+    if (attaque == None):
+        print("ne pouvant pas lancez un sort notre pokemon s'elance a pleine vitesse tete la premiere")
+        degat = math.floor((math.floor(math.floor(((math.floor(int(p1.get_niveau()) * 0.4 + 2) * p1.get_atk_n() * 150) / p2.get_def_n()) / 50)) + 2) * 1)
+        p2.set_hp(p2.get_hp()- degat)
+        p1.set_hp(p1.get_hp()- int(degat/4))
+        print("une attaque de "+str(degat) +" degats")
+    # attaque physique
+    elif (attaque.get_categorie() == 0 and attaque.get_cible() == 1): #verifiez si les conditions sont correctent
+        #transforme les hp du defenseur 
+        p2.set_hp(p2.get_hp()-math.floor((math.floor(math.floor(((math.floor(p1.get_niveau() * 0.4 + 2) * p1.get_atk_n * attaque.get_puissance())/p2.get_def_n)/50)) +2)*cm))
+    # attaque special
+    elif (attaque.get_categorie() == 0 and attaque.get_cible() == 1): #verifiez si les conditions sont correctent
+        #transforme les hp du defenseur 
+        p2.set_hp(p2.get_hp()-math.floor((math.floor(math.floor(((math.floor(p1.get_niveau() * 0.4 + 2) * p1.get_atk_spe * attaque.get_puissance())/p2.get_def_spe)/50)) +2)*cm))
+    else :
+        print("attaque echoué, better luck next time")
 
 
-
-"""
